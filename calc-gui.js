a267a1fcd348d681735573438a000b2f21ea044c
imivory <imivory38@gmail.com>
2020-10-19 08:59:08 +0900

    js 계산기

---
+++ calc-gui.js
@@ -0,0 +1,8 @@
+var clickNumbers = function(event){
+    console.log(event.target.innerHTML);
+}
+
+var clickOthers = function(event){
+    console.log("click others");
+    console.log(event.target.innerHTML);
+}
