970c390485d9d5672e6dae901d529dc086f0f1a2
imivory <imivory38@gmail.com>
2020-10-29 12:00:29 +0900

    글자퍼즐 완료

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,74 +1,140 @@
-var word1 = document.getElementById("word1");
-var word2 = document.getElementById("word2");
-var check = document.getElementById("check");
+var word1 = document.getElementById("word1"); //answer
+var word2 = document.getElementById("word2"); //buttons
+var check = document.getElementById("check"); // word1 === word2
+var progress = document.getElementById("progress");//progress check
+var time = document.getElementById("time");
+// game objects
+var game = {
+    'btns': [],
+    'maxPlay': 3,
+    'current': 0
+};
+game.startTime = Date.now();
 
-var words = 'apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend'.split(',');
+game.words = 'apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend'.split(',');
 
-var game = {};
-game.choice = function(){
-    var idx = Math.floor(Math.random()*words.length);
-    return words[idx];
+// choose 1 word form words
+game.choose = function () {
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
+
+game.addButtons = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement("button");
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
+    }
 }
 
-var answer = game.choice();
-word1.innerHTML = answer;
-
-game.word = answer.split("");
-game.btns = [];
-
-game.updateDisplay = function(){
-    if(answer === game.word.join('')){
+game.removeButtons = function(){
+    for(var i=0; i<this.btns.length; i++){
+        word2.removeChild(this.btns[i]);
+    }
+    this.btns = [];
+}
+game.checkGood = function(){
+    return this.answer == this.letters.join('');
+}
+game.updateDisplay = function () {
+    if (this.checkGood()) {
         check.innerHTML = '일치합니다';
-    }else{
+    } else {
         check.innerHTML = '일치하지 않습니다.';
     }
 };
 
-for (var i = 0; i < answer.length; i++) {
-    var btn = document.createElement("button");
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
+game.init = function () {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
 }
+game.init();
 
-var swap = function (event) {
+
+game.copyBtnText = function () {
+    for (var i = 0; i < game.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i];
+    }
+};
+
+game.swap = function () {
     var temp = [];
-    while(game.word.length != 0){
-        var s = game.word.pop();
+    while (game.letters.length != 0) {
+        var s = game.letters.pop();
         temp.push(s);
     }
-    game.word = temp;
+    game.letters = temp;
     game.copyBtnText();
     game.updateDisplay();
 };
 
-game.copyBtnText = function(){
-    for (var i = 0; i < game.word.length; i++){
-        this.btns[i].innerHTML = this.word[i];
+game.rshift = function () {
+    var s = game.letters.pop();
+    game.letters.unshift(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+game.lshift = function () {
+    var s = game.letters.shift();
+    game.letters.push(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.progress = function(){
+    if(game.checkGood()){
+        game.current++;
+        game.removeButtons();
+        game.init();
+        game.shuffle();
+        var str = "";
+        for(var i = 0; i < game.current;i++){
+            str += "O";
+        }
+        progress.innerHTML = str;
+    }
+    if(game.current == game.maxPlay){
+        var sec = (Date.now() - game.startTime)/1000;
+        alert("Good! Your Record : "+sec+"sec");
+        clearInterval(x);
+        check.innerHTML = "Thanks for playing";
     }
 };
-
-var rshift = function (event) {
-    var s = game.word.pop();
-    game.word.unshift(s);
-    game.copyBtnText();
+// event hadler for swap button
+var swap = function () {
+    game.swap();
+    game.progress();
 };
 
-var lshift = function (event) {
-    var s = game.word.shift();
-    game.word.push(s);
-    game.copyBtnText();
+var rshift = function () {
+    game.rshift();
+    game.progress();
 };
-
+var lshift = function () {
+    game.lshift();
+    game.progress();
+};
 // shuffle
+game.shuffle = function () {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
+        game.swap();
+    }
 
-var toggle = Math.floor(Math.random()*2) === 0;
-if (toggle){
-    swap();
+    var n = Math.floor(Math.random() * (game.answer.length - 1));
+    for (var i = 0; i < n; i++) {
+        game.rshift();
+    }
+};
+game.shuffle();
+
+var updateTime = function(){
+    var now = Date.now() - game.startTime;
+    time.innerHTML = (now / 1000) + "s";
 }
 
-var n = Math.floor(Math.random()* answer.length);
-
-for (var i = 0; i< n; i++){
-    rshift();
-}
+var x = setInterval(updateTime, 50);
